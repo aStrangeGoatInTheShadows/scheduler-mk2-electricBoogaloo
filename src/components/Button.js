beac5f43931926207f3aae69ad52cbb732fe1802
Matt Macdonald <mksmacdonald@gmail.com>
2021-08-03 09:41:41 -0700

    Removed skips from button test

--- src/components/Button.js
+++ src/components/Button.js
@@ -3,20 +3,23 @@ import React from "react";
 import "components/Button.scss";
 
 export default function Button(props) {
-   let buttonClass = "button";
+  let buttonClass = "button";
 
-   if (props.confirm) {
-     buttonClass += " button--confirm";
-   }
- 
+  if (props.confirm) {
+    buttonClass += " button--confirm";
+  }
 
-   if (props.danger) {
-      buttonClass += " button--danger";
-    }
+  if (props.danger) {
+    buttonClass += " button--danger";
+  }
 
-   return <button onClick={props.onClick}
-   disabled={props.disabled}
-   className={buttonClass}>
-      {props.children}</button>;
+  return (
+    <button
+      onClick={props.onClick}
+      disabled={props.disabled}
+      className={buttonClass}
+    >
+      {props.children}
+    </button>
+  );
 }
-

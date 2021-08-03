ccbf445f7bf4231fb309474052468eafc4c67325
Matt Macdonald <mksmacdonald@gmail.com>
2021-08-03 13:56:59 -0700

    logic to format spots remaining

--- src/components/DayListItem.js
+++ src/components/DayListItem.js
@@ -1,10 +1,31 @@
 import React from "react";
+import classNames from "classnames/bind";
+
+import "components/DayListItem.scss";
+import { tsPropertySignature } from "@babel/types";
 
 export default function DayListItem(props) {
+  const formatSpots = () => {
+    if (!props.spots) {
+      return "no spots remaining";
+    }
+
+    if (props.spots === 1) {
+      return "1 spot remaining";
+    }
+
+    return `${props.spots} spots remaining`;
+  };
+
+  let dayListItemClass = classNames("day-list__item", {
+    "day-list__item--selected": props.selected,
+    "day-list__item--full": !props.spots,
+  });
+
   return (
-    <li>
-      <h2 className="text--regular">Day Name</h2>
-      <h3 className="text--light">X spots remaining</h3>
+    <li className={dayListItemClass}>
+      <h2 className="text--regular">{props.name}</h2>
+      <h3 className="text--light">{formatSpots()}</h3>
     </li>
   );
 }

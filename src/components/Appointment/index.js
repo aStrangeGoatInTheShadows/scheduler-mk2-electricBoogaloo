a3dd8db87327c7ac8039783d38bd0fdba6ffb9c7
Matt Macdonald <mksmacdonald@gmail.com>
2021-08-03 18:12:28 -0700

    init

---
+++ src/components/Appointment/index.js
@@ -0,0 +1,11 @@
+import React from "react";
+import classNames from "classnames/bind";
+
+import "./styles.scss";
+
+export default function Appointment(props) {
+  return (
+    // <li className={dayListItemClass} onClick={props.setDay(props.name)}>
+    <li className={dayListItemClass}></li>
+  );
+}

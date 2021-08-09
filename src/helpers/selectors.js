be644b513f6f7e0ccb1c15d7d84a2d6b0c35457e
Matt Macdonald <mksmacdonald@gmail.com>
2021-08-09 09:54:37 -0700

    uncommented function to make tests past - should be removed

--- src/helpers/selectors.js
+++ src/helpers/selectors.js
@@ -24,23 +24,23 @@ const getInterviewerNameById = (state, id) => {
   return state.interviewers[id].name;
 };
 
-// // When passed the state and day, the function returns
-// // a specific interview?
-// const getInterviewersForDay = (state, selectedDay) => {
-//   const interviewersForDay = [];
-//   let appointments = [];
+// When passed the state and day, the function returns
+// a specific interview?
+const getInterviewersForDay = (state, selectedDay) => {
+  const interviewersForDay = [];
+  let appointments = [];
 
-//   for (let day of state.days) {
-//     if (day.name === selectedDay) {
-//       appointments = [...day.appointments];
-//     }
-//   }
+  for (let day of state.days) {
+    if (day.name === selectedDay) {
+      appointments = [...day.appointments];
+    }
+  }
 
-//   for (let index of appointments) {
-//     interviewersForDay.push(state.appointments[index]);
-//   }
+  for (let index of appointments) {
+    interviewersForDay.push(state.appointments[index]);
+  }
 
-//   return interviewersForDay;
-// };
+  return interviewersForDay;
+};
 
-export { getAppointmentsForDay, getInterviewerNameById };
+export { getAppointmentsForDay, getInterviewerNameById, getInterviewersForDay };

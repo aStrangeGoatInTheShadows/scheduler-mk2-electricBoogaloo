6d47904bebce7bcbd359d7a4ac0ccf37db1a16ce
Matt Macdonald <mksmacdonald@gmail.com>
2021-08-09 22:18:23 -0700

    init

---
+++ src/components/__tests__/Appointment.test.js
@@ -0,0 +1,21 @@
+import React from "react";
+
+import { render, cleanup } from "@testing-library/react";
+
+import Appointment from "components/Appointment";
+
+afterEach(cleanup);
+
+describe("Appointment", () => {
+  it("renders without crashing", () => {
+    render(
+      <Appointment
+        key={1}
+        appointment={{}}
+        state={{}}
+        onSave={() => {}}
+        onDelete={() => {}}
+      ></Appointment>
+    );
+  });
+});
